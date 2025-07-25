5823ad46241889cce436133b8d2a6cd082434798
DeepakBhargav787860 <Deepak.Bhargav787860@gmail.com>
2025-07-25 10:46:27 +0530

    hhhhhh

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors(corsOrigin()));
 // Middleware
 app.use(express.json());
+app.set('trust proxy', true);
 //matric
 app.use(matricMiddleware)
 //logger
